cd1b5f2442f741a6b82f4cb311b079f996871741
cmfAndez <86676541+cmfAndez@users.noreply.github.com>
2023-02-03 19:31:46 +0100

    Adds Grab Input

--- Assets/_Project/Scripts/RootsBasic/HandController.cs
+++ Assets/_Project/Scripts/RootsBasic/HandController.cs
@@ -6,13 +6,19 @@ public class HandController : MonoBehaviour
 {
     public Camera targetCamera;
     public Transform handVis;
+    public float handDistance;
 
     public GameObject openHand;
     public GameObject closedHand;
     public GameObject closedHandWithRoot;
 
+    public RaycastHit hitInfo;
+    public LayerMask rootsLayer;
+
     private void Start()
     {
+        //Cursor.visible = false;
+        Application.targetFrameRate = 60;
         if (targetCamera == null)
         {
             targetCamera = Camera.main;
@@ -24,34 +30,32 @@ public class HandController : MonoBehaviour
         PlayerHand();
         //Mouse Input [left = 0, right = 1, middle = 2]
         MouseInput(Input.GetMouseButton(0));
+        Grab();
     }
 
     private void PlayerHand()
     {
         Ray inputRay = targetCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(inputRay, out RaycastHit hitInfo))
+        if (Physics.Raycast(inputRay, out hitInfo))
         {
-            if (!handVis.gameObject.activeInHierarchy)
-            {
-                handVis.gameObject.SetActive(true);
-            }
             Vector3 contactPoint = hitInfo.point;
-            handVis.position = contactPoint;
-        }
-        else if (handVis.gameObject.activeInHierarchy)
-        {
-            handVis.gameObject.SetActive(false);
+            handVis.position = contactPoint + (-handDistance * inputRay.direction);
         }
     }
 
     private void MouseInput(bool mousePress)
     {
-        if (handVis.gameObject.activeInHierarchy)
-        {
-            openHand.SetActive(!mousePress);
+        openHand.SetActive(!mousePress);
+        closedHand.SetActive(mousePress);
+    }
 
-            closedHand.SetActive(mousePress);
+    private void Grab()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider[] hits = Physics.OverlapSphere(hitInfo.point, 1, rootsLayer);
+
         }
     }
 }
